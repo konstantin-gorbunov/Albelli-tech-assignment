2df8aa831c109a29fce2a3aa0dadf8f25eabadab
Kostya Gorbunov <kgorbunov@viber.com>
2019-06-29 11:25:49 +0200

    - initial commit

---
+++ EnhanceWrapper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EnhanceWrapper
+//
+//  Created by Kostiantyn Gorbunov on 29/06/2019.
+//  Copyright © 2019 Kostiantyn Gorbunov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EnhanceWrapper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EnhanceWrapper
+//
+//  Created by Kostiantyn Gorbunov on 29/06/2019.
+//  Copyright © 2019 Kostiantyn Gorbunov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
